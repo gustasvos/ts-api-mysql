d0bd6a05c626e89e3adef71b88b73365adf6e58b
Gustavo Rosa <gustavorib.rosa@gmail.com>
2023-03-21 01:05:36 -0300

    jwt token login route

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { userRepository } from '../repositories/userRepository';
 import { BadRequestError } from "../helpers/api-errors";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class UserController {
     async create(req: Request, res: Response) {
@@ -10,7 +11,7 @@ export class UserController {
         const hashPassword = await bcrypt.hash(password, 10);
         const userExists = await userRepository.findOneBy({ email });
 
-        if (userExists) throw new BadRequestError('Emails already exists');
+        if (userExists) throw new BadRequestError('Email already exists');
 
         const newUser = userRepository.create({
             name,
@@ -23,6 +24,31 @@ export class UserController {
         
         // console.log(newUser.password);
         return res.status(201).json(user);
+    };
+
+    async login(req: Request, res: Response) {
+        const { email, password } = req.body;
+
+        const user = await userRepository.findOneBy({ email });
+        
+        if (!user) {
+            throw new BadRequestError('invalid email or password');
+        }
+        
+        const verifyPassword = await bcrypt.compare(password, user.password);
+
+        if (!verifyPassword) {
+            throw new BadRequestError('invalid email or password');
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET ?? '', { expiresIn: '1h'});
+
+        const { password: _, ...userLogin } = user;
+
+        return res.json({
+            user: userLogin,
+            token: token
+        });
     };
 
 };
